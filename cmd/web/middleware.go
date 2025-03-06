71777c093389f22558e3bb56960c42610224606a
Ayush <hackerboy5556@gmail.com>
2025-03-06 15:56:54 +0530

    separation of resources for authenticated and not authenticated users

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -28,6 +28,18 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 	})
 }
 
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAuthenticated(r) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+
+		w.Header().Set("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
